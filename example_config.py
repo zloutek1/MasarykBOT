518cd820759cb7aa603fcd7ac8da2db4dbca33e5
Tomáš Ljutenko <xljutenk@fi.muni.cz>
2019-07-20 16:16:40 +0200

    example configuration file

---
+++ example_config.py
@@ -0,0 +1,19 @@
+
+class BotConfig:
+    prefix = "<your-prefix>"
+    token = "<your-token-here>"
+
+    db_config = {
+        "host": "localhost",
+        "user": "root",
+        "passwd": "",
+        "database": "discord"
+    }
+
+    error_rooms = (
+        123456789,
+    )
+
+    reactionmenu_channels = (
+        123456789,
+    )
