9cbbb83196eecbe1f4e0ee8b80b537ba06cd0923
Tomáš Ljutenko <xljutenk@fi.muni.cz>
2019-07-20 16:13:33 +0200

    generalized taskManager

--- core/taskManager.py
+++ core/taskManager.py
@@ -13,23 +13,28 @@ class TaskManager(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-        self.tasklist = {60: []}
+        self.tasklist = {}  # every_seconds: [tasks]
         self.bot.add_background_task = self.add_background_task
 
-        self.bot.loop.create_task(self.oneMinuteTasks())
-
-    async def oneMinuteTasks(self):
+    async def runTaskEvery(self, seconds: int):
         await self.bot.wait_until_ready()
 
         while not self.bot.is_closed():
             # do stuff
-            for task in self.tasklist[60]:
+            for task in self.tasklist[seconds]:
                 await task()
 
-            await asyncio.sleep(60)
+            await asyncio.sleep(seconds)
 
-    def add_background_task(self, task, timer=60):
-        self.tasklist[timer].append(task)
+    def add_background_task(self, task, repeat_every: int = 60):
+        assert repeat_every % 5 == 0, "time has to be divisible by 5"
+
+        if repeat_every in self.tasklist:
+            self.tasklist[repeat_every].append(task)
+
+        else:
+            self.tasklist[repeat_every] = [task]
+            self.bot.loop.create_task(self.runTaskEvery(seconds=repeat_every))
 
 
 def setup(bot):
