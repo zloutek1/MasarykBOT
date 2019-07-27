210dcf97b3913e0eba8ed500592e7f10213b9e73
Tomáš Ljutenko <xljutenk@fi.muni.cz>
2019-07-27 10:45:55 +0200

    indented into __name__==__main__

--- __main__.py
+++ __main__.py
@@ -4,22 +4,25 @@ from discord.ext.commands import when_mentioned_or
 from core.bot import MasarykBot
 from config import BotConfig
 
-bot = MasarykBot(
-    command_prefix=when_mentioned_or(BotConfig.prefix),
-    activity=Game(name="Commands: !help"),
-    case_insensitive=True,
-    db_config=BotConfig.db_config
-)
+if __name__ == "__main__":
+    bot = MasarykBot(
+        command_prefix=when_mentioned_or(BotConfig.prefix),
+        activity=Game(name="Commands: !help"),
+        case_insensitive=True,
+        db_config=BotConfig.db_config
+    )
 
-# Internal/debug
-bot.load_extension("core.events")
-bot.load_extension("core.taskManager")
-bot.load_extension("core.errors")
-bot.load_extension("core.admin")
+    # Internal/debug
+    print("---[ Internal ]---")
+    bot.load_extension("core.events")
+    bot.load_extension("core.taskManager")
+    # bot.load_extension("core.errors")
+    bot.load_extension("core.admin")
 
-# Commands
-bot.load_extension("cogs.picker")
-bot.load_extension("cogs.antispam")
+    # Commands
+    print("\n---[ Commands ]---")
+    bot.load_extension("cogs.reactionPicker")
+    bot.load_extension("cogs.antispam")
+    bot.load_extension("cogs.backup")
 
-
-bot.start(BotConfig.token)
+    bot.start(BotConfig.token)
