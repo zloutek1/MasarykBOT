0d237d65ee4e26b7c865d9b19506e48b3b5a7d3a
Tomáš Ljutenko <xljutenk@fi.muni.cz>
2019-07-30 21:23:38 +0200

    added verification setup command

--- cogs/verification.py
+++ cogs/verification.py
@@ -2,14 +2,40 @@ import discord
 from discord.ext import commands
 from discord import Colour, Embed, Member, Object, File
 
+import core.utils.get
+
 
 class Verification(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def verify(self, ctx):
-        print(ctx.author.name)
+    @commands.group(name="verification")
+    async def verication_group(self, ctx):
+        pass
+
+    @verication_group.command(name="setup")
+    async def verification_setup(self, ctx, message_id: int):
+        await ctx.message.delete()
+
+        guild = ctx.guild
+        channel = ctx.channel
+        message = await channel.fetch_message(message_id)
+
+        ctx.db.execute("INSERT INTO verification (guild_id, channel_id, message_id) VALUES (%s, %s, %s)", (guild.id, channel.id, message.id))
+
+        await message.add_reaction(core.utils.get(self.bot.emojis, name="Verification"))
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        guild = self.bot.get_guild(payload.guild_id)
+        author = guild.get_member(payload.user_id)
+        channel = self.bot.get_channel(payload)
+
+        if author.bot:
+            return
+
+        studentRole = core.utils.get(guild.roles, name="Student")
+        await author.add_roles(studentRole)
 
 
 def setup(bot):
