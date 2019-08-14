1008aeecb594ed217559b4cb287a9f2a1fd95c7a
Zloutek1 <xljutenk@fi.muni.cz>
2019-08-14 12:14:54 +0200

    modified @needs_database to work with the new database library

--- core/utils/checks.py
+++ core/utils/checks.py
@@ -1,5 +1,8 @@
+import aiomysql
 from discord.ext import commands
 from functools import wraps
+
+from core.utils.db import Database
 
 
 def needs_database(func):
@@ -8,9 +11,17 @@ def needs_database(func):
         if not hasattr(self.bot, "db"):
             return False
 
-        self.db = self.bot.db.connect()
-        if not self.db:
-            return False
+        if hasattr(self, "db") and not self.db.cursor.closed:
+            return await func(self, *args, **kwargs)
 
-        return await func(self, *args, **kwargs)
+        pool = self.bot.db.pool
+        if not pool:
+            return
+
+        async with pool.acquire() as conn:
+            async with conn.cursor(aiomysql.DictCursor) as cursor:
+                self.db = Database(conn, cursor, pool)
+
+                retval = await func(self, *args, **kwargs)
+        return retval
     return wrapper
