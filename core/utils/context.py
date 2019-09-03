1429056820fdf31b3bcdce4453643745fb166fd9
Zloutek1 <xljutenk@fi.muni.cz>
2019-09-03 15:51:54 +0200

    added shorthand get functions

--- core/utils/context.py
+++ core/utils/context.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import asyncio
 import discord
 
+import core.utils.get
 from core.utils import db
 from core.utils.db import Database
 
@@ -11,3 +12,12 @@ from config import BotConfig
 class Context(commands.Context):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def get_channel(self, name):
+        return core.utils.get(self.guild.channels, name=name)
+
+    def get_role(self, name):
+        return core.utils.get(self.guild.roles, name=name)
+
+    def get_emoji(self, name):
+        return core.utils.get(self.bot.emojis, name=name)
