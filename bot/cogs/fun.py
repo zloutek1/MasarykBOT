ba4920bda7792e2b3a6507806bd0dcc51c09b922
Tomáš Ljutenko <xljutenk@fi.muni.cz>
2019-09-04 10:40:43 +0200

    removed source command and fixed typo

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -24,7 +24,7 @@ class Fun(commands.Cog):
             await ctx.send(" ".join(list(map(str, emojis[i:i + 10]))))
 
         if len(emojis) == 0:
-            await ctx.send("This guild has not emojis")
+            await ctx.send("This guild has no emojis")
 
     """--------------------------------------------------------------------------------------------------------------------------"""
 
@@ -98,50 +98,6 @@ class Fun(commands.Cog):
         e = discord.Embed()
         e.add_field(name=choice(("Yes", "No")), value=question)
         await ctx.send(embed=e)
-
-    @commands.command()
-    async def source(self, ctx, *, command: str = None):
-        """Displays my full source code or for a specific command.
-        To display the source code of a subcommand you can separate it by
-        periods, e.g. tag.create for the create subcommand of the tag command
-        or by spaces.
-        """
-
-        def extract_wrapped(decorated):
-            closure = (c.cell_contents for c in decorated.__closure__)
-            return next((c for c in closure if isinstance(c, FunctionType)), None)
-
-        source_url = 'https://gitlab.com/zloutek1/MasarykBOT'
-        branch = 'master'
-        if command is None:
-            return await ctx.send(source_url)
-
-        if command == 'help':
-            src = type(self.bot.help_command)
-            module = src.__module__
-            filename = inspect.getsourcefile(src)
-        else:
-            obj = self.bot.get_command(command.replace('.', ' '))
-            if obj is None:
-                return await ctx.send('Could not find command.')
-
-            # since we found the command we're looking for, presumably anyway, let's
-            # try to access the code itself
-            src = extract_wrapped(obj.callback).__code__
-            module = obj.callback.__module__
-            filename = src.co_filename
-
-        lines, firstlineno = inspect.getsourcelines(src)
-        if not module.startswith('discord'):
-            # not a built-in command
-            location = os.path.relpath(filename).replace('\\', '/')
-        else:
-            location = module.replace('.', '/') + '.py'
-            source_url = 'https://github.com/Rapptz/discord.py'
-            branch = 'master'
-
-        final_url = f'<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>'
-        await ctx.send(final_url)
 
     """--------------------------------------------------------------------------------------------------------------------------"""
 
