821481119e035b045fd7ff1f4f8013d2b9c204da
Tomáš Ljutenko <xljutenk@fi.muni.cz>
2019-09-08 22:17:49 +0200

    added get_category other get_ functions support also other kwargs

--- bot/core/utils/context.py
+++ bot/core/utils/context.py
@@ -3,21 +3,24 @@ import asyncio
 import discord
 
 import core.utils.get
-from core.utils import db
-from core.utils.db import Database
-
-from config import BotConfig
 
 
 class Context(commands.Context):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def get_channel(self, name):
-        return core.utils.get(self.guild.channels, name=name)
+    def get_category(self, name=None, **kwargs):
+        kwargs.update({"name": name}) if name is not None else None
+        return core.utils.get(self.guild.categories, **kwargs)
 
-    def get_role(self, name):
-        return core.utils.get(self.guild.roles, name=name)
+    def get_channel(self, name=None, **kwargs):
+        kwargs.update({"name": name}) if name is not None else None
+        return core.utils.get(self.guild.channels, **kwargs)
 
-    def get_emoji(self, name):
-        return core.utils.get(self.bot.emojis, name=name)
+    def get_role(self, name=None, **kwargs):
+        kwargs.update({"name": name}) if name is not None else None
+        return core.utils.get(self.guild.roles, **kwargs)
+
+    def get_emoji(self, name=None, **kwargs):
+        kwargs.update({"name": name}) if name is not None else None
+        return core.utils.get(self.bot.emojis, **kwargs)
