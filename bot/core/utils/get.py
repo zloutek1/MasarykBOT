898491b183347045920e853fa83ccba5c814fe0e
Tomáš Ljutenko <xljutenk@fi.muni.cz>
2019-09-09 22:38:21 +0200

    rewritten utils.get for better readability, now also accepts key=lambda function

--- bot/core/utils/get.py
+++ bot/core/utils/get.py
@@ -4,17 +4,24 @@ import discord.utils
 
 class GetModule(sys.modules[__name__].__class__):
     def __call__(self, iterable, **attrs):
-        result = list(filter(lambda item: all(
-            (
-                hasattr(item, attr) and
-                getattr(item, attr) == attrs[attr]
-            ) or (
-                isinstance(item, dict) and
-                attr in item and item[attr] == attrs[attr]
-            ) for attr in attrs), iterable))
-        return (result if len(result) > 1 else
-                result[0] if len(result) == 1 else
-                None)
+        result = []
+        for item in iterable:
+            matches = True
+            for attr, val in attrs.items():
+                if attr == "key" and isinstance(val, type(lambda: 0)):
+                    matches &= val(item)
+                elif isinstance(item, dict):
+                    matches &= item.get(attr) == val
+                else:
+                    matches &= getattr(item, attr) == val
+            if matches:
+                result.append(item)
+
+        if len(result) == 0:
+            return None
+        if len(result) == 1:
+            return result[0]
+        return result
 
 
 sys.modules[__name__].__class__ = GetModule
