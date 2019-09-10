717559b909beedf0f2e4e0fb7293a6b051e84ba8
Tomáš Ljutenko <xljutenk@fi.muni.cz>
2019-09-10 23:00:44 +0200

    updated example config file

--- bot/example_config.py
+++ bot/example_config.py
@@ -1,17 +1,18 @@
 
+
 class BotConfig:
     prefix = "<your-prefix>"
     name = "<bot-name>"
     icon = "<bot-image-path>"
     token = "<your-token-here>"
 
-    db_config = {
+    mysql = {
         "host": "localhost",
-        "user": "root",
-        "passwd": "",
-        "database": "discord",
-        "use_unicode": True,
-        "autocommit": False
+        "user": "devuser",
+        "password": "devpass",
+        "db": "devdatabase",
+        "charset": "utf8mb4",
+        "connect_timeout": 60
     }
 
     def __getattr__(self, attr):
