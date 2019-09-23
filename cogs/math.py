294f5a5cf5c105c67c2b5a9014c2f61c3e862c98
Tomáš Ljutenko <xljutenk@fi.muni.cz>
2019-09-23 20:30:50 +0200

    fixed typo

--- cogs/math.py
+++ cogs/math.py
@@ -8,17 +8,17 @@ class Math(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def get_math_equasion(self, equasion):
+    async def get_math_equation(self, equation):
         tex2imgURL = "http://www.sciweavers.org/tex2img.php?eq={}&bc=White&fc=Black&im=png&fs=12&ff=arev&edit=0"
 
         embed = discord.Embed()
-        embed.set_image(url=tex2imgURL.format(urllib.parse.quote(equasion)))
+        embed.set_image(url=tex2imgURL.format(urllib.parse.quote(equation)))
 
         return embed
 
     @commands.command()
-    async def latex(self, ctx, *equasion):
-        embed = await self.get_math_equasion(equasion)
+    async def latex(self, ctx, *equation):
+        embed = await self.get_math_equation(equation)
         await ctx.send(embed=embed)
 
 
