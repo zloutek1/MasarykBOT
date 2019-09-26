147eef8c8649b0b5fdbb455217741e92f6a4d3b5
Tomáš Ljutenko <xljutenk@fi.muni.cz>
2019-09-26 18:57:43 +0200

    extended allowed import packages removed global variables

--- assets/eval_template.py
+++ assets/eval_template.py
@@ -1,11 +1,15 @@
 import asyncio
 import importlib
-import pickle
 
 
-def secure_importer(name, globals=None, locals=None, fromlist=(), level=0):
+def __restricted_import__(name, globals=None, locals=None, fromlist=(), level=0):
 
-    allowed = ["asyncio.events", "time"]
+    allowed = ("asyncio.events",
+               'math', 'random', 'time', 'datetime',
+               'functools', 'itertools', 'operator', 'string',
+               'collections', 're', 'json',
+               'heapq', 'bisect', 'copy', 'hashlib')
+
     frommodule = globals['__name__'] if globals else None
 
     if frommodule not in allowed and frommodule + "." + name not in allowed and name not in allowed:
@@ -14,10 +18,13 @@ def secure_importer(name, globals=None, locals=None, fromlist=(), level=0):
     return importlib.__import__(name, globals, locals, fromlist, level)
 
 
-__builtins__.__dict__['__import__'] = secure_importer
+__builtins__.__dict__['__import__'] = __restricted_import__
 
 
 async def func():
+    global asyncio, importlib
+    del asyncio
+    del importlib
 {body}
 
 ret = asyncio.get_event_loop().run_until_complete(func())
