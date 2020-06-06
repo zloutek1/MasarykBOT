1848eb48a9273c83a8052c840618541f6cddb7ea
Tomáš Ljutenko <xljutenk@muni.cz>
2020-06-06 17:00:53 +0200

    removed clutter prints

--- __main__.py
+++ __main__.py
@@ -8,39 +8,18 @@ from dotenv import load_dotenv
 
 if __name__ == "__main__":
     load_dotenv()
-    print()
 
     bot = MasarykBot(
         command_prefix=when_mentioned_or(os.getenv("PREFIX")),
         case_insensitive=True
     )
 
+    # load cogs
     with open("assets/loaded_cogs.json", "r") as fileR:
         modules = json.load(fileR)
 
-    ##
-    # print boot message
-    ##
-    boot_message = """
-    Boot sequence initialised
+        for module in modules:
+            bot.load_extension(module)
 
-    Seeking cogs...
-    {} cogs found
-
-    Loading cogs...""".format(len(modules))
-    for letter in boot_message.strip("\n"):
-        print(letter, end="")
-    print()
-
-    ##
-    # load cogs
-    ##
-    for module in modules:
-        bot.load_extension(module)
-    print("    Loaded:", ", ".join(bot.cogs.keys()) or "No cogs found")
-    print()
-
-    ##
     # start bot
-    ##
     bot.start(os.getenv("TOKEN"))
