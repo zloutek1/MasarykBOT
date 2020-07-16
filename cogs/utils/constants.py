b70559643daf82ec9e5d4eb7fe3973771805eca5
Tomáš Ljutenko <xljutenk@muni.cz>
2020-07-17 01:39:40 +0200

    constats containing ids of channels, roles, emojis, etc

---
+++ cogs/utils/constants.py
@@ -0,0 +1,29 @@
+
+# Channel IDs
+verification_channels = [731525269214724116]
+about_you_channels = [619887144579629069]
+
+
+# Log Channel IDs
+error_log_channels = []
+mute_log_channels = []
+other_log_channels = []
+
+
+# <emoji, role>
+about_you_roles = {
+    617745093889359872: 614438242955165698,     # bot_development
+}
+
+
+# Emoji server
+emojis = {
+    "Verification": 617745093889359872
+}
+
+
+# Roles
+muted_roles = []
+verified_roles = [605751951644295198]
+moderator_roles = []
+admin_roles = []
