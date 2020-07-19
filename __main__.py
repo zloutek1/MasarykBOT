a00e189a06c7dcc0c4864738314136dcba99e480
Tomáš Ljutenko <xljutenk@muni.cz>
2020-07-19 23:47:50 +0200

    added required ENV variable checks before running the bot

--- __main__.py
+++ __main__.py
@@ -15,6 +15,14 @@ if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     log = logging.getLogger()
 
+    if os.getenv("POSTGRES") is None:
+        log.exception("postgresql connection is required to run the bot, exiting...")
+        exit()
+
+    if os.getenv("TOKEN") is None:
+        log.exception("discord bot token is required to run the bot, exiting...")
+        exit()
+
     try:
         pool = loop.run_until_complete(asyncpg.create_pool(os.getenv("POSTGRES"), command_timeout=60))
     except Exception:
