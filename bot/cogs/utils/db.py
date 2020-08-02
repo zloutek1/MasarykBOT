b0503156deb9623ff7d694ae2d864a12bb9eb770
Tomáš Ljutenko <xljutenk@muni.cz>
2020-08-02 16:17:32 +0200

    moved schemas straight to db

--- bot/cogs/utils/db.py
+++ bot/cogs/utils/db.py
@@ -1,8 +1,6 @@
 import asyncio
 import asyncpg
 
-from . import schemas
-
 
 class Table:
     def __init__(self, db):
@@ -40,7 +38,18 @@ class Guilds(Table):
 
     async def insert(self, data):
         async with self.db.acquire() as conn:
-            await conn.executemany(schemas.SQL_INSERT_GUILD, data)
+            await conn.executemany("""
+                INSERT INTO server.guilds AS g (id, name, icon_url, created_at)
+                VALUES ($1, $2, $3, $4)
+                ON CONFLICT (id) DO UPDATE
+                    SET name=$2,
+                        icon_url=$3,
+                        created_at=$4,
+                        edited_at=NOW()
+                    WHERE g.name<>excluded.name OR
+                          g.icon_url<>excluded.icon_url OR
+                          g.created_at<>excluded.created_at
+            """, data)
 
     async def soft_delete(self, ids):
         async with self.db.acquire() as conn:
@@ -57,7 +66,18 @@ class Categories(Table):
 
     async def insert(self, data):
         async with self.db.acquire() as conn:
-            await conn.executemany(schemas.SQL_INSERT_CATEGORY, data)
+            await conn.executemany("""
+                INSERT INTO server.categories AS c (guild_id, id, name, position, created_at)
+                VALUES ($1, $2, $3, $4, $5)
+                ON CONFLICT (id) DO UPDATE
+                    SET name=$3,
+                        position=$4,
+                        created_at=$5,
+                        edited_at=NOW()
+                    WHERE c.name<>excluded.name OR
+                          c.position<>excluded.position OR
+                          c.created_at<>excluded.created_at
+            """, data)
 
     async def update(self, data):
         await self.insert(data)
@@ -77,7 +97,18 @@ class Roles(Table):
 
     async def insert(self, data):
         async with self.db.acquire() as conn:
-            await conn.executemany(schemas.SQL_INSERT_ROLE, data)
+            await conn.executemany("""
+                INSERT INTO server.roles AS r (guild_id, id, name, color, created_at)
+                VALUES ($1, $2, $3, $4, $5)
+                ON CONFLICT (id) DO UPDATE
+                    SET name=$3,
+                        color=$4,
+                        created_at=$5,
+                        edited_at=NOW()
+                    WHERE r.name<>excluded.name OR
+                          r.color<>excluded.color OR
+                          r.created_at<>excluded.created_at
+            """, data)
 
     async def soft_delete(self, ids):
         async with self.db.acquire() as conn:
@@ -94,7 +125,18 @@ class Members(Table):
 
     async def insert(self, data):
         async with self.db.acquire() as conn:
-            await conn.executemany(schemas.SQL_INSERT_USER, data)
+            await conn.executemany("""
+                INSERT INTO server.users AS u (id, names, avatar_url, created_at)
+                VALUES ($1, ARRAY[$2], $3, $4)
+                ON CONFLICT (id) DO UPDATE
+                    SET names=array_prepend($2::varchar, u.names),
+                        avatar_url=$3,
+                        created_at=$4,
+                        edited_at=NOW()
+                    WHERE $2<>ANY(u.names) OR
+                          u.avatar_url<>excluded.avatar_url OR
+                          u.created_at<>excluded.created_at
+            """, data)
 
     async def soft_delete(self, ids):
         async with self.db.acquire() as conn:
@@ -112,7 +154,18 @@ class Channels(Table):
 
     async def insert(self, data):
         async with self.db.acquire() as conn:
-            await conn.executemany(schemas.SQL_INSERT_CHANNEL, data)
+            await conn.executemany("""
+                INSERT INTO server.channels AS ch (guild_id, category_id, id, name, position, created_at)
+                VALUES ($1, $2, $3, $4, $5, $6)
+                ON CONFLICT (id) DO UPDATE
+                    SET name=$4,
+                        position=$5,
+                        created_at=$6,
+                        edited_at=NOW()
+                    WHERE ch.name<>excluded.name OR
+                          ch.position<>excluded.position OR
+                          ch.created_at<>excluded.created_at
+            """, data)
 
     async def update(self, data):
         await self.insert(data)
@@ -132,7 +185,17 @@ class Messages(Table):
 
     async def insert(self, messages):
         async with self.db.acquire() as conn:
-            await conn.executemany(schemas.SQL_INSERT_MESSAGE, messages)
+            await conn.executemany("""
+                INSERT INTO server.messages AS m (channel_id, author_id, id, content, created_at, edited_at)
+                VALUES ($1, $2, $3, $4, $5, $6)
+                ON CONFLICT (id) DO UPDATE
+                    SET content=$4,
+                        created_at=$5,
+                        edited_at=$6
+                    WHERE m.content<>excluded.content OR
+                          m.created_at<>excluded.created_at OR
+                          m.edited_at<>excluded.edited_at
+            """, messages)
 
     async def update(self, messages):
         await self.insert(messages)
@@ -148,7 +211,15 @@ class Attachments(Table):
 
     async def insert(self, attachments):
         async with self.db.acquire() as conn:
-            await conn.executemany(schemas.SQL_INSERT_ATTACHEMNT, attachments)
+            await conn.executemany("""
+                INSERT INTO server.attachments AS a (message_id, id, filename, url)
+                VALUES ($1, $2, $3, $4)
+                ON CONFLICT (id) DO UPDATE
+                    SET filename=$3,
+                        url=$4
+                    WHERE a.filename<>excluded.filename OR
+                          a.url<>excluded.url
+            """, attachments)
 
 
 class Reactions(Table):
@@ -162,7 +233,11 @@ class Reactions(Table):
 
     async def insert(self, reactions):
         async with self.db.acquire() as conn:
-            await conn.executemany(schemas.SQL_INSERT_REACTIONS, reactions)
+            await conn.executemany("""
+                INSERT INTO server.reactions AS r (message_id, name, member_ids)
+                VALUES ($1, $2, $3)
+                ON CONFLICT (message_id, name) DO NOTHING
+            """, reactions)
 
 
 class Emojis(Table):
@@ -177,7 +252,11 @@ class Emojis(Table):
 
     async def insert(self, emojis):
         async with self.db.acquire() as conn:
-            await conn.executemany(schemas.SQL_INSERT_EMOJIS, emojis)
+            await conn.executemany("""
+                INSERT INTO server.emojis AS r (message_id, name, count)
+                VALUES ($1, $2, $3)
+                ON CONFLICT (message_id, name) DO NOTHING
+            """, emojis)
 
 
 class Logger(Table):
@@ -210,7 +289,27 @@ class Leaderboard(Table):
     async def preselect(self, guild_id, ignored_users, channel_id):
         async with self.db.acquire() as conn:
             await conn.execute("DROP TABLE IF EXISTS ldb_lookup")
-            await conn.execute(schemas.SQL_SELECT_LEADERBOARD, guild_id, ignored_users, channel_id)
+            await conn.execute("""
+                CREATE TEMPORARY TABLE IF NOT EXISTS ldb_lookup AS
+                    SELECT
+                        ROW_NUMBER() OVER (ORDER BY sent_total DESC), *
+                    FROM (
+                        SELECT
+                            author_id,
+                            author.names[1] AS author,
+                            SUM(messages_sent) AS sent_total
+                        FROM cogs.leaderboard
+                        INNER JOIN server.users AS author
+                            ON author_id = author.id
+                        INNER JOIN server.channels AS channel
+                            ON channel_id = channel.id
+                        WHERE guild_id = $1::bigint AND
+                              author_id<>ALL($2::bigint[]) AND
+                              ($3::bigint IS NULL OR channel_id = $3)
+                        GROUP BY author_id, author.names
+                        ORDER BY sent_total DESC
+                    ) AS lookup
+            """, guild_id, ignored_users, channel_id)
 
     async def get_top10(self):
         async with self.db.acquire() as conn:
@@ -218,7 +317,27 @@ class Leaderboard(Table):
 
     async def get_around(self, author_id):
         async with self.db.acquire() as conn:
-            return await conn.fetch(schemas.SQL_AROUND_SELECT_LEADERBOARD, author_id)
+            return await conn.fetch("""
+                WITH desired_count AS (
+                    SELECT sent_total
+                    FROM ldb_lookup
+                    WHERE author_id = $1
+                )
+
+                (   SELECT *
+                    FROM ldb_lookup
+                    WHERE sent_total >= (SELECT * FROM desired_count) AND author_id <> $1
+                    ORDER BY sent_total LIMIT 2
+                ) UNION (
+                    SELECT *
+                    FROM ldb_lookup
+                    WHERE sent_total = (SELECT * FROM desired_count)
+                ) UNION (
+                    SELECT *
+                    FROM ldb_lookup
+                    WHERE sent_total < (SELECT * FROM desired_count) AND author_id <> $1 LIMIT 2
+                ) ORDER BY sent_total DESC
+            """, author_id)
 
 
 class Emojiboard(Table):
@@ -231,7 +350,20 @@ class Emojiboard(Table):
 
     async def select(self, guild_id, ignored_users, channel_id):
         async with self.db.acquire() as conn:
-            return await conn.fetch(schemas.SQL_SELECT_EMOJIBOARD, guild_id, ignored_users, channel_id)
+            return await conn.fetch("""
+                SELECT
+                    emoji.name,
+                    COUNT(*) AS sent_total
+                FROM cogs.emojiboard AS emoji
+                INNER JOIN server.channels AS channel
+                    ON channel_id = channel.id
+                WHERE guild_id = $1::bigint AND
+                      author_id<>ALL($2::bigint[]) AND
+                      ($3::bigint IS NULL OR channel_id = $3)
+                GROUP BY emoji.name
+                ORDER BY sent_total DESC
+                LIMIT 10
+            """, guild_id, ignored_users, channel_id)
 
 
 class DBAcquire:
