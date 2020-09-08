b714a2dadc3172e6794ff18ea8fbffede1683146
Tomáš Ljutenko <zloutek1@gmail.com>
2020-09-08 23:55:47 +0200

    added specific permissions

--- bot/cogs/info.py
+++ bot/cogs/info.py
@@ -27,7 +27,7 @@ class Info(commands.Cog):
 
     @commands.command()
     async def invite(self, ctx):
-        await ctx.send(f"https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=0")
+        await ctx.send(f"https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=268823632")
 
     @commands.command()
     async def info(self, ctx):
