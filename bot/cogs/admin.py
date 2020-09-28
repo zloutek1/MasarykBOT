9293d9ec25ec357b5d1386c04120b572e68d6618
Tomáš Ljutenko <zloutek1@gmail.com>
2020-09-28 13:04:33 +0200

    added purge and shutdown commands

--- bot/cogs/admin.py
+++ bot/cogs/admin.py
@@ -1,4 +1,9 @@
+import logging
+
 from discord.ext import commands
+from discord.ext.commands import has_permissions
+
+log = logging.getLogger(__name__)
 
 class Admin(commands.Cog):
     def __init__(self, bot):
@@ -9,6 +14,18 @@ class Admin(commands.Cog):
     async def say(self, ctx, *, content: str):
         await ctx.send(content)
 
+    @commands.command()
+    @has_permissions(administrator=True)
+    async def purge(self, ctx, limit: int = 0):
+        await ctx.channel.purge(limit=limit + 1)
+
+    @commands.command()
+    @has_permissions(administrator=True)
+    @commands.is_owner()
+    async def shutdown(self, ctx):
+        log.info("Shutting down...")
+        await ctx.message.delete()
+        raise KeyboardInterrupt
 
 def setup(bot):
     bot.add_cog(Admin(bot))
