7459fb50ab98fbc7b8ba6da479e73bac5dfa574d
Tomáš Ljutenko <zloutek1@gmail.com>
2021-01-28 19:09:46 +0100

    added types

--- bot/cogs/utils/constants.example.py
+++ bot/cogs/utils/constants.example.py
@@ -1,35 +1,36 @@
+from typing import List, Dict
 
 # Channel IDs
-verification_channels = []
-about_you_channels = []
-subject_registration_channels = []
-starboard_channels = []
+verification_channels: List[int] = []
+about_you_channels: List[int]  = []
+subject_registration_channels: List[int]  = []
+starboard_channels: List[int]  = []
 
 
 # Log Channel IDs
-error_log_channels = []
-mute_log_channels = []
-other_log_channels = []
+error_log_channels: List[int] = []
+mute_log_channels: List[int] = []
+other_log_channels: List[int] = []
 
 
 # <emoji_name, emoji_id>
-emojis = {}
+emojis: Dict[str, int] = {}
 
 
 # Role IDs
-muted_roles = []
-verified_roles = []
-moderator_roles = []
-admin_roles = []
-show_all_subjects_roles = []
-mute_roles = []
+muted_roles: List[int] = []
+verified_roles: List[int] = []
+moderator_roles: List[int] = []
+admin_roles: List[int] = []
+show_all_subjects_roles: List[int] = []
+mute_roles: List[int] = []
 
 
 # Misc
-NEEDED_REACTIONS = 10
-FAME_REACT_LIMIT = 10
-DEBUG = False
+NEEDED_REACTIONS: int = 10
+FAME_REACT_LIMIT: int = 10
+DEBUG: bool = False
 
 
 # Colors
-MUNI_YELLOW = 0xEACD59
+MUNI_YELLOW: int = 0xEACD59
