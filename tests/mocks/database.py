2c1728b357b8bfa0f8cec5d8783bf4550a8a2fb3
Tomáš Ljutenko <zloutek1@gmail.com>
2021-02-10 19:36:58 +0100

    reworked typecheck, reworked MockDatabase, added MockSubjectRecord

--- tests/mocks/database.py
+++ tests/mocks/database.py
@@ -1,5 +1,7 @@
 import collections
 import unittest.mock
+from typing import Optional, List, Union, get_origin, get_args
+import datetime
 
 from bot.cogs.utils import db
 from asyncpg.protocol.protocol import _create_record as Record
@@ -19,25 +21,49 @@ def record(func):
             3: '3rd',
         }.get(n, f'{n}th')
 
+    def type_check(kwargs):
+        for i, (field, ftype) in enumerate(func.__annotations__.items()):
+            if get_origin(ftype) is None and not isinstance(kwargs[field], ftype):
+                raise TypeError(f"Couldn't match expected type {ftype} with actual type {type(kwargs[field])} in {pos(i+1)} argument {field}")
+
+            elif get_origin(ftype) is not None:
+                if not type_check_generic(kwargs[field], ftype):
+                    raise TypeError(f"Couldn't match expected type {ftype} with actual type {type(kwargs[field])} in {pos(i+1)} argument {field}")
+
+
+    def type_check_generic(value, ftype):
+        if get_origin(ftype) is Union:
+            return any(type_check_generic(value, vtype) for vtype in get_args(ftype))
+
+        if get_origin(ftype) is list:
+            vtype = get_args(ftype)[0]
+            return isinstance(value, list) and all(type_check_generic(inner, vtype) for inner in value)
+
+        return isinstance(value, ftype)
+
     def wrapper(**kwargs):
         """
         require all key-word arguments to be filled and have required type
         """
-        missing = []
-        for i, (field, ftype) in enumerate(func.__annotations__.items()):
-            if field not in kwargs:
-                missing.append(field)
-            elif not isinstance(kwargs[field], ftype):
-                raise TypeError(f"Couldn't match expected type {ftype} with actual type {type(kwargs[field])} in {pos(i+1)} argument {field}")
+        kwargs = func.__kwdefaults__ | kwargs
+
+        missing = [field for field in func.__annotations__ if field not in kwargs]
         if missing:
             raise TypeError(f"{func.__qualname__} missing {len(missing)} required keyword-only arguments: {', '.join(missing)}")
 
-        elems = tuple(kwargs.values())
+        type_check(kwargs)
+
+        elems = tuple(kwargs[key] for key in mapping)
         return Record(mapping, elems)
     return wrapper
 
 @record
 def MockLeaderboardRecord(*, row_number: int, author_id: int, author: str, sent_total: int):
+    pass
+
+@record
+def MockSubjectRecord(*, faculty: str, code: str, name: str, url: str, terms: List[str],
+                         created_at: datetime.datetime, edited_at: Optional[datetime.datetime] = None, deleted_at: Optional[datetime.datetime] = None):
     pass
 
 class MockPool(unittest.mock.MagicMock):
@@ -48,10 +74,13 @@ def MockDatabase():
 
     tables = [table for table in vars(database).values() if isinstance(table, db.Table)]
     for table in tables:
-        table.select = unittest.mock.AsyncMock()
-        table.insert = unittest.mock.AsyncMock()
-        table.update = unittest.mock.AsyncMock()
-        table.delete = unittest.mock.AsyncMock()
-        table.soft_delete = unittest.mock.AsyncMock()
+        for attr in dir(table):
+            if attr.startswith('_'):
+                continue
+
+            if attr in ['prepare', 'prepare_one']:
+                continue
+
+            setattr(table, attr, unittest.mock.AsyncMock())
 
     return database
