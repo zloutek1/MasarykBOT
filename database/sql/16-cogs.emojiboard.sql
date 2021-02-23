8f44b18f0f3bcf57df9ef5fe4588732df3cd75e6
Tomáš Ljutenko <zloutek1@gmail.com>
2021-02-23 20:06:31 +0000

    changed from materialized view to table with triggers

--- database/sql/16-cogs.emojiboard.sql
+++ database/sql/16-cogs.emojiboard.sql
@@ -1,22 +1,99 @@
--- View: cogs.emojiboard
+-- Table: cogs.emojiboard
 
--- DROP MATERIALIZED VIEW cogs.emojiboard;
+-- DROP TABLE cogs.emojiboard;
 
-CREATE MATERIALIZED VIEW cogs.emojiboard
-TABLESPACE pg_default
-AS
- SELECT m.channel_id,
-    unnest(reactions.member_ids) AS author_id,
-    reactions.name
-   FROM server.reactions
-     JOIN server.messages m ON reactions.message_id = m.id
-UNION ALL
- SELECT m.channel_id,
-    m.author_id,
-    emojis.name
-   FROM server.emojis
-     JOIN server.messages m ON emojis.message_id = m.id
-WITH DATA;
+CREATE TABLE cogs.emojiboard
+(
+    channel_id bigint,
+    author_id bigint,
+    name text COLLATE pg_catalog."default",
+    count integer
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
 
 ALTER TABLE cogs.emojiboard
-    OWNER TO masaryk;
+    OWNER to masaryk_dev;
+-- Index: emojiboard_idx_unique
+
+-- DROP INDEX cogs.emojiboard_idx_unique;
+
+CREATE UNIQUE INDEX emojiboard_idx_unique
+    ON cogs.emojiboard USING btree
+    (channel_id ASC NULLS LAST, author_id ASC NULLS LAST, name COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default;
+
+
+
+
+-- FUNCTION: server.update_emojiboard_emoji()
+
+-- DROP FUNCTION server.update_emojiboard_emoji();
+
+CREATE FUNCTION server.update_emojiboard_emoji()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
+BEGIN
+	INSERT INTO cogs.emojiboard AS eb (channel_id, author_id, name, count)
+	SELECT channel_id, author_id, NEW.name, 1
+		FROM server.messages WHERE server.messages.id = NEW.message_id
+	ON CONFLICT (channel_id, author_id, name) DO UPDATE
+		SET count = eb.count + 1;
+	RETURN NEW;
+END
+$BODY$;
+
+ALTER FUNCTION server.update_emojiboard_emoji()
+    OWNER TO masaryk_dev;
+
+
+-- Trigger: update_emojiboard
+
+-- DROP TRIGGER update_emojiboard ON server.emojis;
+
+CREATE TRIGGER update_emojiboard
+    AFTER INSERT
+    ON server.emojis
+    FOR EACH ROW
+    EXECUTE PROCEDURE server.update_emojiboard_emoji();
+
+
+
+
+-- FUNCTION: server.update_emojiboard_reaction()
+
+-- DROP FUNCTION server.update_emojiboard_reaction();
+
+CREATE FUNCTION server.update_emojiboard_reaction()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
+BEGIN
+	INSERT INTO cogs.emojiboard AS eb (channel_id, author_id, name, count)
+	SELECT channel_id, unnest(NEW.member_ids) as author_id, NEW.name, 1
+		FROM server.messages WHERE server.messages.id = NEW.message_id
+	ON CONFLICT (channel_id, author_id, name) DO UPDATE
+		SET count = eb.count + 1;
+	RETURN NEW;
+END
+$BODY$;
+
+ALTER FUNCTION server.update_emojiboard_reaction()
+    OWNER TO masaryk_dev;
+
+-- Trigger: update_emojiboard
+
+-- DROP TRIGGER update_emojiboard ON server.reactions;
+
+CREATE TRIGGER update_emojiboard
+    AFTER INSERT
+    ON server.reactions
+    FOR EACH ROW
+    EXECUTE PROCEDURE server.update_emojiboard_reaction();
