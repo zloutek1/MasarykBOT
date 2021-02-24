64aaaa189933046b0e32616318d052786f0cd31e
Tomáš Ljutenko <zloutek1@gmail.com>
2021-02-24 18:42:15 +0000

    added more tests

--- tests/bot/cogs/test_verification.py
+++ tests/bot/cogs/test_verification.py
@@ -1,8 +1,8 @@
 import unittest
-from unittest.mock import Mock, AsyncMock, patch
+from unittest.mock import Mock, AsyncMock, PropertyMock, patch, call
 
 from bot.cogs import verification
-from tests.mocks.discord import MockBot, MockGuild, MockRole, MockMember, MockTextChannel, MockEmoji, MockRawReactionActionEvent
+from tests.mocks.discord import MockBot, MockGuild, MockRole, MockMember, MockTextChannel, MockMessage, MockReaction, MockEmoji, MockRawReactionActionEvent, AsyncIterator
 
 class VerificationTests(unittest.IsolatedAsyncioTestCase):
     async def test_verification_channels(self):
@@ -21,13 +21,82 @@ class VerificationTests(unittest.IsolatedAsyncioTestCase):
         #
 
         bot = MockBot()
-        cog = verification.Verification(bot=bot)
         bot.get_channel = lambda id: MockTextChannel(id=id)
+        cog = verification.Verification(bot=bot)
 
         with patch('bot.cogs.verification.Config', config):
             excepted = [MockTextChannel(id=123), MockTextChannel(id=456), MockTextChannel(id=789)]
             actual = cog.verification_channels
             self.assertCountEqual(excepted, actual)
+
+    async def test_synchronize(self):
+        #
+        reaction = MockReaction(emoji=MockEmoji(name="Verification"))
+        message = MockMessage(reactions=[reaction])
+
+        guild = MockGuild(id=1)
+
+        channel = MockTextChannel(id=123, guild=guild)
+        channel.history.return_value = AsyncIterator([message])
+
+        bot = MockBot()
+        #
+
+        with patch('bot.cogs.verification.Verification.verification_channels', new_callable=PropertyMock) as verification_channels:
+            verification_channels.return_value = [channel]
+
+            cog = verification.Verification(bot=bot)
+            cog._synchronize_react = AsyncMock(return_value=None)
+            await cog._synchronize()
+            cog._synchronize_react.assert_called_once_with(guild, reaction)
+
+    async def test_synchronize_react(self):
+        #
+        guild = Mock()
+        guild.id = 1
+        guild.roles.verified = 2
+
+        config = Mock()
+        config.guilds = [guild]
+        #
+
+        #
+        role = MockRole(id=2)
+
+        members_with_role = [
+            MockMember(id=3, roles=[role]),
+            MockMember(id=4, roles=[role]),
+            MockMember(id=5, roles=[role])
+        ]
+
+        users_that_reacted = [
+            MockMember(id=3, roles=[role]),
+            MockMember(id=4, roles=[role]),
+            MockMember(id=6, roles=[]),
+        ]
+
+        guild = MockGuild(id=1, roles=[role], members=members_with_role)
+
+        react = MockReaction()
+        react.users.return_value = AsyncIterator(users_that_reacted)
+        #
+
+        bot = MockBot()
+        cog = verification.Verification(bot=bot)
+        cog._verify_join = AsyncMock(return_value=None)
+        cog._verify_leave = AsyncMock(return_value=None)
+
+        with patch('bot.cogs.verification.Config', config):
+            await cog._synchronize_react(guild, react)
+
+        cog._verify_join.assert_has_calls([
+            call(MockMember(id=6, roles=[]))
+        ])
+
+
+        cog._verify_leave.assert_has_calls([
+            call(MockMember(id=5, roles=[role])),
+        ])
 
     async def test_reaction_add(self):
         payload = MockRawReactionActionEvent(
