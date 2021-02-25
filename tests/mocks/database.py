39cca11259bdedc15027fee30c76220771cf66c3
Tomáš Ljutenko <zloutek1@gmail.com>
2021-02-25 18:16:26 +0000

    changed @record decorator to work same as @dataclass decorator

--- tests/mocks/database.py
+++ tests/mocks/database.py
@@ -1,83 +1,19 @@
+import sys
 import collections
-import unittest.mock
-from functools import wraps
-from typing import Optional, List, Union, get_origin, get_args
 import datetime
+from functools import wraps
+from dataclasses import dataclass
 
-from bot.cogs.utils import db
+import unittest.mock
+
+
+from typing import Optional, List, Union, get_origin, get_args
+
+
+#from bot.cogs.utils import db
 from asyncpg.protocol.protocol import _create_record as Record
 
-def record(func):
-    """
-    return a Record with column names from annotated variables
-    """
-
-    fields = func.__annotations__.keys()
-    mapping = collections.OrderedDict((val, idx) for (idx, val) in enumerate(fields))
-
-    def pos(n):
-        return {
-            1: '1st',
-            2: '2nd',
-            3: '3rd',
-        }.get(n, f'{n}th')
-
-    def type_check(kwargs):
-        for i, (field, ftype) in enumerate(func.__annotations__.items()):
-            if get_origin(ftype) is None and not isinstance(kwargs[field], ftype):
-                raise TypeError(f"Couldn't match expected type {ftype} with actual type {type(kwargs[field])} in {pos(i+1)} argument {field}")
-
-            elif get_origin(ftype) is not None:
-                if not type_check_generic(kwargs[field], ftype):
-                    raise TypeError(f"Couldn't match expected type {ftype} with actual type {type(kwargs[field])} in {pos(i+1)} argument {field}")
-
-
-    def type_check_generic(value, ftype):
-        if get_origin(ftype) is Union:
-            return any(type_check_generic(value, vtype) for vtype in get_args(ftype))
-
-        if get_origin(ftype) is list:
-            vtype = get_args(ftype)[0]
-            return isinstance(value, list) and all(type_check_generic(inner, vtype) for inner in value)
-
-        return isinstance(value, ftype)
-
-    @wraps(func)
-    def wrapper(**kwargs):
-        """
-        require all key-word arguments to be filled and have required type
-        """
-        kwargs = func.__kwdefaults__ | kwargs if func.__kwdefaults__ is not None else kwargs
-
-        missing = [field for field in func.__annotations__ if field not in kwargs]
-        if missing:
-            raise TypeError(f"{func.__qualname__} missing {len(missing)} required keyword-only arguments: {', '.join(missing)}")
-
-        type_check(kwargs)
-
-        elems = tuple(kwargs[key] for key in mapping)
-        return Record(mapping, elems)
-    return wrapper
-
-@record
-def MockLeaderboardRecord(*, row_number: int, author_id: int, author: str, sent_total: int):
-    pass
-
-@record
-def MockSubjectRecord(*, faculty: str, code: str, name: str, url: str, terms: List[str],
-                         created_at: datetime.datetime, edited_at: Optional[datetime.datetime] = None, deleted_at: Optional[datetime.datetime] = None):
-    pass
-
-@record
-def MockSubjectRegisteredRecord(*, faculty: str, code: str, guild_id: int, member_ids: List[int]):
-    pass
-
-@record
-def MockSubjectServerRecord(*, faculty: str, code: str, guild_id: int, category_id: Optional[int], channel_id: int, voice_channel_id: Optional[int]):
-    pass
-
-class MockPool(unittest.mock.MagicMock):
-    pass
+"""
 
 def MockDatabase():
     database = db.Database(MockPool())
@@ -94,3 +30,138 @@ def MockDatabase():
             setattr(table, attr, unittest.mock.AsyncMock())
 
     return database
+"""
+def record(cls=None, /, *, new=True):
+    """
+    @record decorator
+    this decorator generates a new __new__ function that
+    returns a valid Record instance with the fields
+    specified in the decorated class
+
+    This decorator has been inspired by the
+    @dataclass decorator
+    """
+
+    def wrap(cls):
+        return _process_class(cls, new)
+
+    if cls is None:
+        # We're called with parens.
+        return wrap
+
+    # We're called as @record without parens.
+    return wrap(cls)
+
+
+def _process_class(cls, new):
+    if cls.__module__ in sys.modules:
+        globals = sys.modules[cls.__module__].__dict__
+    else:
+        globals = {}
+
+    # Annotated fields defined in this class will be used
+    # to generate internal functions
+    cls_fields = cls.__dict__.get('__annotations__', {})
+
+    # Now find fields in our class.  While doing so, validate some
+    # things, and set the default values (as class attributes) where
+    # we can.
+    #cls_fields = [field for field in cls_annotations]
+
+    if new:
+        # The name to use for the "cls"
+        # param in __new__.  Use "cls"
+        # if possible.
+        cls_name = '__dataclass_cls__' if 'cls' in cls_fields else 'cls'
+        self_name = '__dataclass_self__' if 'self' in cls_fields else 'self'
+
+        # create __new__ function that returns a record
+        setattr(cls, '__new__', _new_fn(cls_name, cls_fields, globals=globals))
+        setattr(cls, '__init__', _init_fn(self_name, cls_fields, globals=globals))
+
+    return cls
+
+def _new_fn(cls_name, fields, *, globals=None):
+    """
+    create a __new__ function that has cls as first arguemnt
+    and fields as required-keyword arguments
+    return a Record instance from asyncpg module
+    """
+
+    body_lines = []
+
+    body_lines.append("mapping = collections.OrderedDict({" + ", ".join(f"'{f}': {i}" for i, f in enumerate(fields)) + "})")
+    body_lines.append('elems = (' + ", ".join(f for f in fields) + ')')
+    body_lines.append("return Record(mapping, elems)")
+
+    return _create_fn('__new__', cls_name, fields, body_lines, globals=globals)
+
+def _init_fn(self_name, fields, *, globals=None):
+    return _create_fn('__init__', self_name, fields, [], globals=globals)
+
+
+def _create_fn(name, cls_name, fields, body_lines, *, return_type=None, globals=None):
+
+    if not body_lines:
+        body_lines = ["pass"]
+
+    args = ', '.join(f"{field_name}: {field_type.__qualname__}" for (field_name, field_type) in fields.items())
+    body = '\n'.join(f'  {b}' for b in body_lines)
+
+    txt = f'def {name}({cls_name}, {args}) -> {return_type}:\n{body}'
+
+    ns = {}
+    exec(txt, globals, ns)
+    return ns[name]
+
+
+
+
+
+
+
+
+class MockPool(unittest.mock.MagicMock):
+    pass
+
+
+@record
+@dataclass
+class MockLeaderboardRecord:
+    row_number: int
+    author_id: int
+    author: str
+    sent_total: int
+
+
+@record
+@dataclass
+class MockSubjectRecord:
+    faculty: str
+    code: str
+    name: str
+    url: str
+    terms: List[str]
+    created_at: datetime.datetime
+    edited_at: Optional[datetime.datetime] = None
+    deleted_at: Optional[datetime.datetime] = None
+
+
+@record
+@dataclass
+class MockSubjectRegisteredRecord:
+    faculty: str
+    code: str
+    guild_id: int
+    member_ids: List[int]
+
+
+@record
+@dataclass
+class MockSubjectServerRecord:
+    faculty: str
+    code: str
+    guild_id: int
+    category_id: Optional[int]
+    channel_id: int
+    voice_channel_id: Optional[int]
