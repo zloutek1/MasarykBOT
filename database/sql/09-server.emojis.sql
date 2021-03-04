d34a6b96965452f2ee59b12fad451103f8e8998f
Tomáš Ljutenko <zloutek1@gmail.com>
2021-03-04 19:26:57 +0000

    removed created_at and deleted_at

--- database/sql/09-server.emojis.sql
+++ database/sql/09-server.emojis.sql
@@ -8,9 +8,7 @@ CREATE TABLE server.emojis
     name character varying COLLATE pg_catalog."default" NOT NULL,
     url character varying COLLATE pg_catalog."default",
     animated boolean,
-    created_at timestamp without time zone NOT NULL DEFAULT now(),
     edited_at timestamp without time zone,
-    deleted_at timestamp without time zone,
     CONSTRAINT emojis_pkey PRIMARY KEY (id)
 )
 WITH (
