b14c5718f44adaeed7be9d36497585182fb94330
Tomáš Ljutenko <zloutek1@gmail.com>
2021-03-06 07:53:34 +0000

    updated unique constraint

--- database/sql/14-muni.subject_category.sql
+++ database/sql/14-muni.subject_category.sql
@@ -46,5 +46,5 @@ CREATE INDEX fki_subject_category_fkey_guild
 
 CREATE UNIQUE INDEX subject_category_idx_unique
     ON muni.subject_category USING btree
-    (code COLLATE pg_catalog."default" ASC NULLS LAST, guild_id ASC NULLS LAST)
+    (faculty COLLATE pg_catalog."default" ASC NULLS LAST, code COLLATE pg_catalog."default" ASC NULLS LAST, guild_id ASC NULLS LAST)
     TABLESPACE pg_default;
