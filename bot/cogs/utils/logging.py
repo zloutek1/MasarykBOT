fd51defb3c26c6e709f37b4298b94108bac8c248
Tomáš Ljutenko <485503@mail.muni.cz>
2021-11-17 19:25:57 +0100

    added time-rotated log files

--- bot/cogs/utils/logging.py
+++ bot/cogs/utils/logging.py
@@ -1,9 +1,17 @@
 import logging
 import os
+from logging import FileHandler, Formatter
+from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
 from rich.logging import RichHandler
 
+def my_namer(default_name):
+    # This will be called when doing the log rotation
+    # default_name is the default filename that would be assigned, e.g. Rotate_Test.txt.YYYY-MM-DD
+    # Do any manipulations to that name here, for example this changes the name to Rotate_Test.YYYY-MM-DD.txt
+    base_filename, ext, date = default_name.split(".")
+    return f"{base_filename}.{date}.{ext}"
 
 def setup_logging():
     """
@@ -23,11 +31,12 @@ def setup_logging():
 
     filename = Path("logs", "bot.log")
     os.makedirs(os.path.dirname(filename), exist_ok=True)
-    all_file_handler = logging.FileHandler(filename, mode='a')
+    all_file_handler = TimedRotatingFileHandler(filename, when='midnight')
+    all_file_handler.namer = my_namer
 
     filename = Path("logs", "warn.log")
     os.makedirs(os.path.dirname(filename), exist_ok=True)
-    warn_file_handler = logging.FileHandler(filename, mode='a')
+    warn_file_handler = FileHandler(filename, mode='a')
 
     log.setLevel(logging.DEBUG)
     shell_handler.setLevel(logging.INFO)
@@ -38,9 +47,9 @@ def setup_logging():
     fmt_shell = '{message}'
     fmt_file = '{asctime} | {levelname:<7} | {filename:>20}:{lineno:<4} | {message}'
 
-    shell_handler.setFormatter(logging.Formatter(fmt_shell, fmt_date, style='{'))
-    all_file_handler.setFormatter(logging.Formatter(fmt_file, fmt_date, style='{'))
-    warn_file_handler.setFormatter(logging.Formatter(fmt_file, fmt_date, style='{'))
+    shell_handler.setFormatter(Formatter(fmt_shell, fmt_date, style='{'))
+    all_file_handler.setFormatter(Formatter(fmt_file, fmt_date, style='{'))
+    warn_file_handler.setFormatter(Formatter(fmt_file, fmt_date, style='{'))
 
     log.addHandler(shell_handler)
     log.addHandler(all_file_handler)
