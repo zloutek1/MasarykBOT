9789e520060b3d366d25d9783007e0ab283d04cb
Tomáš Ljutenko <485503@mail.muni.cz>
2022-02-25 19:34:18 +0000

    added partial typing to markov
    redis-json requires fixing types

--- bot/cogs/markov.py
+++ bot/cogs/markov.py
@@ -3,11 +3,16 @@ import functools
 import logging
 import random
 from enum import Enum
-from typing import Callable, Coroutine, List, Tuple
+from typing import Any, Callable, List, Tuple, cast
 
+import inject
+from bot.cogs.utils.context import Context
+from bot.db.messages import MessageDao
+from bot.db.utils import Record
 from disnake.ext import commands
 from disnake.ext.commands import has_permissions
 from disnake.utils import escape_mentions
+from redis import Redis
 from redis.commands.json.path import Path
 
 log = logging.getLogger(__name__)
@@ -24,24 +29,27 @@ class MarkovState(Enum):
     READY = "READY"
     TRAINING = "TRAINING"
 
-def to_thread(func: Callable) -> Coroutine:
+def to_thread(func: Callable[..., Any]) -> Callable[..., Any]:
     @functools.wraps(func)
-    async def wrapper(*args, **kwargs):
+    async def wrapper(*args: Any, **kwargs: Any) -> Any:
         return await asyncio.to_thread(func, *args, **kwargs)
     return wrapper
 
 class Markov(commands.Cog):
-    def __init__(self, bot):
+    messageDao = MessageDao()
+    redis = cast(Redis[str], inject.attr('redis'))
+
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
         self.state = MarkovState.UNINITIALIZED
-        self.possible_starts = []
+        self.possible_starts: List[NGram] = []
 
-        assert self.bot.redis is not None, "this cog requires redis to function"
 
     @commands.group(invoke_without_command=True)
-    async def markov(self, ctx, *_anything):
+    async def markov(self, ctx: Context, *_anything: str) -> None:
         if self.state != MarkovState.READY:
-            return await ctx.send_error(f"markov is not ready, current state is {self.state}")
+            await ctx.send_error(f"markov is not ready, current state is {self.state}")
+            return
 
         i = 0
         while i < 100:
@@ -51,27 +59,33 @@ class Markov(commands.Cog):
             else:
                 break
         else:
-            return await ctx.send_error(f"markov chain simulation timed out :(")
+            await ctx.send_error(f"markov chain simulation timed out :(")
+            return
 
         await ctx.reply(escape_mentions(self.to_message(state)))
 
     @markov.command(aliases=['retrain'])
     @has_permissions(administrator=True)
-    async def train(self, ctx):
+    async def train(self, ctx: Context) -> None:
         await ctx.send("[Markov] Training ...")
 
         success = await self._train()
         if not success:
-            return await ctx.send_error(f"markov is not ready, current state is {self.state}")
+            await ctx.send_error(f"markov is not ready, current state is {self.state}")
+            return
 
         await ctx.reply("[Markov] Finished training")
 
     @commands.Cog.listener()
-    async def on_ready(self):
-        messages = await self.bot.db.messages.select_all_long()
-        self.possible_starts = [
-            (SOF, message.get('content').split(maxsplit=1)[0])
-            for message in messages]
+    async def on_ready(self) -> None:
+        messages = await self.messageDao.select_all_long()
+
+        self.possible_starts = []
+        for message in messages:
+            assert message.get('content') is not None, "ERROR: inconstant database"
+            next = cast(str, message.get('content')).split(maxsplit=1)[0]
+            self.possible_starts.append((SOF, next))
+
         self.state = MarkovState.READY
 
     def to_message(self, parts: List[str]) -> str:
@@ -84,9 +98,9 @@ class Markov(commands.Cog):
         return self.simulate_from(start)
 
     def simulate_from(self, ngram: NGram) -> List[str]:
-        assert N == 2
+        assert N == 2, "ERROR: expected Ngram size 2"
 
-        options = self.bot.redis.json().get("markov." + SEP.join(ngram))
+        options = self.redis.json().get("markov." + SEP.join(ngram))
         if options is None or len(options.keys()) == 0:
             return [EOF]
 
@@ -95,29 +109,34 @@ class Markov(commands.Cog):
 
         return [ngram[0]] + self.simulate_from(next_ngram)
 
-    async def _train(self):
+    async def _train(self) -> bool:
         if self.state not in (MarkovState.UNINITIALIZED, MarkovState.READY):
             return False
+
         self.state = MarkovState.TRAINING
         log.info("Markov training started")
 
-        messages = await self.bot.db.messages.select_all_long()
+        messages = await self.messageDao.select_all_long()
         for i, message in enumerate(messages):
             percentage = int((i + 1) / len(messages) * 100)
             if int((i) / len(messages) * 100) < percentage:
                 log.info(f"Training at {percentage}%")
 
-            if len(message.get('content').strip()) == 0:
+            assert message.get('content') is not None, "ERROR: inconstant database"
+            content = cast(str, message.get('content'))
+
+            if len(content.strip()) == 0:
                 continue
 
-            line = f"{SOF} {message.get('content')} {EOF}"
+            line = f"{SOF} {content} {EOF}"
             await self.markov_train_line(message, line)
 
         log.info("Markov training finished")
         self.state = MarkovState.UNINITIALIZED
+        return True
 
     @to_thread
-    def markov_train_line(self, message, line: str):
+    def markov_train_line(self, message: Record, line: str) -> None:
         words = line.split()
         words = [word for word in words if self.filter_word(message, word)]
         if not words:
@@ -127,12 +146,14 @@ class Markov(commands.Cog):
             ngram = words[i:i+N]
             next = words[i+N]
 
-            options = self.bot.redis.json().get("markov." + SEP.join(ngram)) or {}
+            options = self.redis.json().get("markov." + SEP.join(ngram)) or {}
             options[next] = options.get(next, 0) + 1
-            self.bot.redis.json().set("markov." + SEP.join(ngram), Path.rootPath(), options)
+            self.redis.json().set("markov." + SEP.join(ngram), Path.rootPath(), options)
 
-    def filter_word(self, message, word: str):
-        author = self.bot.get_user(message.get('author_id'))
+    def filter_word(self, message: Record, word: str) -> bool:
+        assert message.get('author_id') is not None, "ERROR: inconstant database"
+
+        author = self.bot.get_user(cast(int, message.get('author_id')))
         return not (
             any(word.startswith(prefix) for prefix in ['!', 'pls']) or
             word.startswith("@") or
@@ -142,5 +163,5 @@ class Markov(commands.Cog):
             author.bot if author else False
         )
 
-def setup(bot):
+def setup(bot: commands.Bot) -> None:
     bot.add_cog(Markov(bot))
