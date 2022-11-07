2a629011c974b5fa74ec3ac81d8b2a52477f32f4
zloutek1 <zloutek1@gmail.com>
2022-11-07 21:06:52 +0000

    refactor: extract markov train code

--- bot/cogs/markov.py
+++ bot/cogs/markov.py
@@ -1,17 +1,12 @@
-import asyncio
-import functools
 import logging
-import random
 from enum import Enum
-from typing import Any, Callable, List, Tuple, cast
+from typing import Dict, Tuple
 
 import inject
 from bot.cogs.utils.context import Context
 from bot.db.messages import MessageDao
-from bot.db.utils import Record
-from disnake.ext import commands
-from disnake.ext.commands import has_permissions
-from disnake.utils import escape_mentions
+from discord.ext import commands
+from discord.ext.commands import has_permissions
 from aioredis import Redis
 
 log = logging.getLogger(__name__)
@@ -23,40 +18,119 @@ SOF = "​SOF​"
 EOF = "​EOF​"
 SEP = "​.​"
 
+
+
 class MarkovState(Enum):
     UNINITIALIZED = "UNINITIALIZED"
     READY = "READY"
     TRAINING = "TRAINING"
 
-class Markov(commands.Cog):
-    messageDao = MessageDao()
+
+
+class MarkovTrainService:
+    messageDao = inject.attr(MessageDao)
     redis = inject.attr(Redis)
+
+
+    async def train(self) -> None:
+        await self.clear_cache()
+
+        messages = await self.messageDao.select_longer_then(30)
+        for message in messages:
+            await self.markov_train_line(message['content'])
+
+
+    async def clear_cache(self):
+        await self.redis.flushall()
+
+
+    async def markov_train_line(self, line: str) -> None:
+        words = line.split()
+        for i in range(len(words) - N):
+            await self._increment_entry(
+                ngram=tuple(words[i:i+N]), 
+                next=words[i+N]
+            )
+
+
+    async def _increment_entry(self, ngram: NGram, next: str):
+        key = "markov." + SEP.join(ngram)
+        options: Dict[str, int] = await self.redis.hgetall(key) or {}
+        options[next] = options.get(next, 0) + 1
+        await self.redis.hset(key, mapping=options)
+
+
+
+class MarkovService(MarkovTrainService):
+    messageDao = inject.attr(MessageDao)
+    redis = inject.attr(Redis)
+
+    def __init__(self) -> None:
+        super(MarkovTrainService).__init__()
+
+    """
+    async def generate_message(self) -> str:
+        self._assert_can_generate_message()
+        
+        state = await self.simulate()
+        message = escape_mentions(self.to_message(state))
+        
+        return message
+
+    def _assert_can_generate_message(self) -> None:
+        if self.state != MarkovState.READY:
+            self._raise_not_ready()
+
+        if not self.possible_starts:
+            raise AssertionError("no possible starts")
+
+    def _raise_not_ready(self) -> None:
+        if self.state == MarkovState.TRAINING:
+            raise AssertionError(f"markov is not ready, current state is {self.state}. Current progress {self.training_progress}%")
+        raise AssertionError(f"markov is not ready, current state is {self.state}")
+
+    @staticmethod
+    def to_message(parts: List[str]) -> str:
+        assert parts[0] == SOF, f"message parts must start with SOF, but was {parts}"
+        assert parts[-1] == EOF, f"message parts must end with EOF, but was {parts}"
+        return " ".join(parts[1:-1])
+    """
+
+
+class Markov(commands.Cog,):
+    service = MarkovService()
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.state = MarkovState.UNINITIALIZED
-        self.possible_starts: List[NGram] = []
-        self.training_progress = 100
 
-        if self.redis is None:
-            raise Exception("redis connection is required")
 
     @commands.group(invoke_without_command=True)
     async def markov(self, ctx: Context, *_anything: str) -> None:
-        if self.state != MarkovState.READY:
-            if self.state == MarkovState.TRAINING:
-                await ctx.send_error(f"markov is not ready, current state is {self.state}. Current progress {self.training_progress}%")
-            else:
-                await ctx.send_error(f"markov is not ready, current state is {self.state}")
-            return
+        try:
+            # message = self.service.generate_message()
+            await ctx.reply("no message")
+        except Exception as ex:
+            await ctx.send_error(str(ex))
 
-        if not self.possible_starts:
-            await ctx.send_error("no possible starts")
-            return
-            
-        state = await self.simulate()
 
-        await ctx.reply(escape_mentions(self.to_message(state)))
+    @markov.command(name='train', aliases=['retrain', 'grind'])
+    @has_permissions(administrator=True)
+    async def _train(self, ctx: Context) -> None:
+        await ctx.send("[Markov] Grinding ...")
+
+        await self.service.train()
+        
+        await ctx.reply("[Markov] Finished grinding")
+
+
+    """
+    @commands.group(invoke_without_command=True)
+    async def markov(self, ctx: Context, *_anything: str) -> None:
+        try:
+            message = self.service.generate_message()
+            await ctx.reply(message)
+        except Exception as ex:
+            await ctx.send_error(str(ex))
 
     @markov.command(name='train', aliases=['retrain', 'grind'])
     @has_permissions(administrator=True)
@@ -96,11 +170,6 @@ class Markov(commands.Cog):
             self.state = MarkovState.READY
             log.info(f"loaded {len(self.possible_starts)} markov starts")
 
-    def to_message(self, parts: List[str]) -> str:
-        assert parts[0] == SOF, f"message parts must start with SOF, but was {parts}"
-        assert parts[-1] == EOF, f"message parts must end with EOF, but was {parts}"
-        return " ".join(parts[1:-1])
-
     async def simulate(self) -> List[str]:
         start = random.choice(self.possible_starts)
         return await self.simulate_from(start)
@@ -119,48 +188,7 @@ class Markov(commands.Cog):
         next_ngram = ngram[1:] + (following,)
 
         return [ngram[0]] + await self.simulate_from(next_ngram)
-
-    async def train(self) -> bool:
-        if self.state not in (MarkovState.UNINITIALIZED, MarkovState.READY):
-            return False
-
-        self.state = MarkovState.TRAINING
-        log.info("Markov training started")
-        self.training_progress = 0
-
-        await self.redis.flushall()
-        
-        messages = await self.messageDao.select_all_long()
-        for i, message in enumerate(messages):
-            percentage = int((i + 1) / len(messages) * 100)
-            if int((i) / len(messages) * 100) < percentage:
-                log.info(f"Training at {percentage}%")
-                self.training_progress = percentage
-
-            if len(message['content'].strip()) == 0:
-                continue
-
-            line = f"{SOF} {message['content']} {EOF}"
-            await self.markov_train_line(message, line)
-
-        log.info("Markov training finished")
-        self.state = MarkovState.UNINITIALIZED
-        return True
-
-    async def markov_train_line(self, message: Record, line: str) -> None:
-        words = line.split()
-        words = [word for word in words if self.filter_word(message, word)]
-        if not words:
-            return
-
-        for i in range(len(words) - N):
-            ngram = words[i:i+N]
-            next = words[i+N]
-
-            options = await self.redis.hgetall("markov." + SEP.join(ngram)) or {}
-            options[next] = int(options.get(next, 0)) + 1
-            await self.redis.hset("markov." + SEP.join(ngram), mapping=options)
-
+    
     def filter_word(self, message: Record, word: str) -> bool:
         author = self.bot.get_user(message['author_id'])
         return not (
@@ -171,6 +199,8 @@ class Markov(commands.Cog):
             "<:@" in word or
             author.bot if author else False
         )
+    """
 
-def setup(bot: commands.Bot) -> None:
-    bot.add_cog(Markov(bot))
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(Markov(bot))
