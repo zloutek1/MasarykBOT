9a6907e42165d3d9f9d1e8b39194eb58dd7acab5
zloutek1 <zloutek1@gmail.com>
2022-11-11 20:28:21 +0000

    refactored auto_thread

--- bot/cogs/auto_thread.py
+++ bot/cogs/auto_thread.py
@@ -1,19 +1,24 @@
+from typing import cast
 from urllib.request import urlopen
 from lxml.html import parse
 
-
-from disnake import (Message)
-from disnake.utils import get
-from disnake.ext import commands
+from discord import (Message)
+from discord.utils import get
+from discord.ext import commands
 
 from bot.constants import Config
+
+
 
 class AutoThread(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
+
     @commands.Cog.listener()
     async def on_message(self, message: Message) -> None:
+        assert message.guild, "can only be used in guilds"
+
         guild_config = get(Config.guilds, id = message.guild.id)
         if guild_config is None:
             return
@@ -27,11 +32,12 @@ class AutoThread(commands.Cog):
             url = title
             page = urlopen(url)
             p = parse(page)
-            title = p.find(".//title").text
+            title = cast(str, p.find(".//title").text)
 
         title = title[:50] + "..." if len(title) > 50 else title
         await message.create_thread(name=title)
 
 
-def setup(bot: commands.Bot) -> None:
-    bot.add_cog(AutoThread(bot))
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(AutoThread(bot))
