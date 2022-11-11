abf3c67855230152202e0b0d65af1cd29c81b80f
zloutek1 <zloutek1@gmail.com>
2022-11-11 20:43:07 +0000

    extract service from auto_thread

--- bot/cogs/auto_thread.py
+++ bot/cogs/auto_thread.py
@@ -2,15 +2,43 @@ from typing import cast
 from urllib.request import urlopen
 from lxml.html import parse
 
-from discord import (Message)
+from discord import Message
+from discord.abc import MessageableChannel
 from discord.utils import get
 from discord.ext import commands
 
-from bot.constants import Config
+from bot.constants import CONFIG
+
+
+class AutoThreadService:
+    @staticmethod
+    def is_threaded_channel(channel: MessageableChannel) -> bool:
+        if not (guild := channel.guild):
+            return False
+
+        if not (guild_config := get(CONFIG.guilds, id = guild.id)):
+            return False
+
+        threaded_channels = guild_config.channels.threaded
+        return channel.id in threaded_channels
+
+
+    @staticmethod
+    def extract_thread_title(message: Message) -> str:
+        title = message.content.split("\n")[0]
+        if "http://" in title or "https://" in title:
+            url = title
+            page = urlopen(url)
+            p = parse(page)
+            title = cast(str, p.find(".//title").text)
+
+        return title[:50] + "..." if len(title) > 50 else title
 
 
 
 class AutoThread(commands.Cog):
+    service = AutoThreadService()
+
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
@@ -19,22 +47,10 @@ class AutoThread(commands.Cog):
     async def on_message(self, message: Message) -> None:
         assert message.guild, "can only be used in guilds"
 
-        guild_config = get(Config.guilds, id = message.guild.id)
-        if guild_config is None:
+        if not self.service.is_threaded_channel(message.channel):
             return
-
-        threaded_channels = guild_config.channels.threaded
-        if message.channel.id not in threaded_channels:
-            return
-
-        title = message.content.split("\n")[0]
-        if "http://" in title or "https://" in title:
-            url = title
-            page = urlopen(url)
-            p = parse(page)
-            title = cast(str, p.find(".//title").text)
-
-        title = title[:50] + "..." if len(title) > 50 else title
+        
+        title = self.service.extract_thread_title(message)
         await message.create_thread(name=title)
 
 
