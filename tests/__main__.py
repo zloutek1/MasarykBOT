e747c0f62e2f8491a5ac9e6a97a720f89f5679ad
zloutek1 <zloutek1@gmail.com>
2022-11-29 04:13:50 +0100

    fix: add global db Pool mock

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,8 +1,21 @@
+import unittest.mock
+
+import inject
 import pytest
+
+from bot.db import Pool
+
+
+def setup_mock_injections():
+    inject.configure(lambda binder: binder.bind(Pool, unittest.mock.Mock(Pool)))
+
 
 def load_tests(loader, tests, pattern):
     return loader.discover('.')
 
+
+
 if __name__ == '__main__':
+    setup_mock_injections()
     exit_code = pytest.main(["--cov-report", "xml:cov.xml", "--cov", ".", "-s"])
     exit(int(exit_code))
