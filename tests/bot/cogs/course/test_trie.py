cd8b2e470bab572962335e9762867c4d325eb516
zloutek1 <zloutek1@gmail.com>
2022-11-29 21:02:38 +0100

    refactor: use assert_that

--- tests/bot/cogs/course/test_trie.py
+++ tests/bot/cogs/course/test_trie.py
@@ -1,42 +1,47 @@
 import unittest.mock
 
+from assertpy import assert_that
+
 from bot.cogs.course.trie import Trie
 
 
 
 class TrieTests(unittest.TestCase):
-    def test_insert_given_empty_word_sets_root_as_word(self):
+    @staticmethod
+    def test_insert_given_empty_word_sets_root_as_word():
         trie = Trie()
         trie.insert("")
-        self.assertTrue(trie.is_word)
+        assert_that(trie.is_word).is_true()
 
 
-    def test_insert_given_word_sets_word_as_word(self):
+    @staticmethod
+    def test_insert_given_word_sets_word_as_word():
         trie = Trie()
         trie.insert("hi")
 
-        self.assertIn('h', trie.children)
-        self.assertFalse(trie.children['h'].is_word)
+        assert_that(trie.children).contains('h')
+        assert_that(trie.children['h'].is_word).is_false()
 
-        self.assertIn('i', trie.children['h'].children)
-        self.assertTrue(trie.children['h'].children['i'].is_word)
+        assert_that(trie.children['h'].children).contains('i')
+        assert_that(trie.children['h'].children['i'].is_word).is_true()
 
 
-    def test_contains_given_existing_word_returns_true(self):
+    @staticmethod
+    def test_contains_given_existing_word_returns_true():
         trie = Trie()
         trie.insert("hi")
+        assert_that(trie.contains('hi')).is_true()
 
-        self.assertTrue(trie.contains('hi'))
 
-
-    def test_contains_given_missing_word_returns_false(self):
+    @staticmethod
+    def test_contains_given_missing_word_returns_false():
         trie = Trie()
         trie.insert("hi")
+        assert_that(trie.contains('hello')).is_false()
 
-        self.assertFalse(trie.contains('hello'))
 
-
-    def test_insert_all_given_two_words_inserts_both(self):
+    @staticmethod
+    def test_insert_all_given_two_words_inserts_both():
         multi_trie = Trie()
         multi_trie.insert_all(["hi", "hello"])
 
@@ -44,54 +49,60 @@ class TrieTests(unittest.TestCase):
         single_trie.insert("hi")
         single_trie.insert("hello")
 
-        self.assertEqual(single_trie, multi_trie)
+        assert_that(single_trie).is_equal_to(multi_trie)
 
 
-    def test_generate_prefix_groups_given_limit_one_returns_five_result(self):
+    @staticmethod
+    def test_generate_prefix_groups_given_limit_one_returns_five_result():
         trie = Trie()
         trie.insert_all(["hello", "hi", "help", "helicopter", "hire", "hindu", "hollow", "ah"])
 
         actual = trie.generate_prefix_groups(1)
-        self.assertEqual(["hell", "heli", "hir", "hin", "ho", "a"], actual)
+        assert_that(actual).contains_only("hell", "heli", "hir", "hin", "ho", "a")
 
 
-    def test_generate_prefix_groups_given_limit_two_returns_five_results(self):
+    @staticmethod
+    def test_generate_prefix_groups_given_limit_two_returns_five_results():
         trie = Trie()
         trie.insert_all(["hello", "hi", "help", "helicopter", "hire", "hindu", "hollow", "ah"])
 
         actual = trie.generate_prefix_groups(2)
-        self.assertEqual(["hell", "heli", "hi", "ho", "a"], actual)
+        assert_that(actual).contains_only("hell", "heli", "hi", "ho", "a")
 
 
-    def test_generate_prefix_groups_given_limit_three_returns_four_results(self):
+    @staticmethod
+    def test_generate_prefix_groups_given_limit_three_returns_four_results():
         trie = Trie()
         trie.insert_all(["hello", "hi", "help", "helicopter", "hire", "hindu", "hollow", "ah"])
 
         actual = trie.generate_prefix_groups(3)
-        self.assertEqual(["he", "hi", "ho", "a"], actual)
+        assert_that(actual).contains_only("he", "hi", "ho", "a")
 
 
-    def test_generate_prefix_groups_given_limit_seven_returns_two_results(self):
+    @staticmethod
+    def test_generate_prefix_groups_given_limit_seven_returns_two_results():
         trie = Trie()
         trie.insert_all(["hello", "hi", "help", "helicopter", "hire", "hindu", "hollow", "ah"])
 
         actual = trie.generate_prefix_groups(7)
-        self.assertEqual(["h", "a"], actual)
+        assert_that(actual).contains_only("h", "a")
 
 
-    def test_find_prefix_with_missing_word_returns_None(self):
+    @staticmethod
+    def test_find_prefix_with_missing_word_returns_None():
         trie = Trie()
         trie.insert_all(["hello", "hi", "help", "helicopter", "hire", "hindu", "hollow", "ah"])
 
         actual = trie.find_prefix_for("alibaba", limit=3)
-        self.assertIsNone(actual)
+        assert_that(actual).is_none()
 
 
-    def test_find_prefix_for_given_limit_three_returns_correct_prefix(self):
+    @staticmethod
+    def test_find_prefix_for_given_limit_three_returns_correct_prefix():
         trie = Trie()
         trie.insert_all(["hello", "hi", "help", "helicopter", "hire", "hindu", "hollow", "ah"])
 
         actual = trie.find_prefix_for("hello", limit=3)
-        self.assertEqual("he", actual)
+        assert_that(actual).is_equal_to("he")
 
 
