efbc125e13cb99f7fafd55c71b622ab51ab73fe6
zloutek1 <zloutek1@gmail.com>
2022-12-01 07:53:15 +0100

    refactor: AttachmentRepository now works on Entity

--- bot/db/discord/attachments.py
+++ bot/db/discord/attachments.py
@@ -1,26 +1,36 @@
+from dataclasses import dataclass, astuple
 from typing import Optional, Sequence, Tuple
 
 from discord import Attachment
 
-from bot.db.tables import ATTACHMENTS
-from bot.db.utils import (Crud, DBConnection, Id, Mapper, Url, inject_conn)
-
-Columns = Tuple[Optional[Id], Id, str, Url]
+from bot.db.utils import Entity, Mapper, Id, Crud, inject_conn, DBConnection
 
 
-class AttachmentMapper(Mapper[Attachment, Columns]):
-    async def map(self, obj: Attachment) -> Columns:
+
+@dataclass
+class AttachmentEntity(Entity):
+    message_id: Optional[Id]
+    id: Id
+    filename: str
+    url: str
+
+
+
+class AttachmentMapper(Mapper[Attachment, AttachmentEntity]):
+    async def map(self, obj: Attachment) -> AttachmentEntity:
         attachment = obj
-        return None, attachment.id, attachment.filename, attachment.url
+        return AttachmentEntity(None, attachment.id, attachment.filename, attachment.url)
 
 
-class AttachmentRepository(Crud[Columns]):
+
+class AttachmentRepository(Crud[AttachmentEntity]):
     def __init__(self) -> None:
-        super().__init__(table_name=ATTACHMENTS)
+        super().__init__(table_name="server.attachments")
+
 
     @inject_conn
-    async def insert(self, conn: DBConnection, data: Sequence[Columns]) -> None:
-        await conn.executemany(f"""
+    async def insert(self, conn: DBConnection, data: AttachmentEntity) -> None:
+        await conn.execute(f"""
             INSERT INTO server.attachments AS a (message_id, id, filename, url)
             VALUES ($1, $2, $3, $4)
             ON CONFLICT (id) DO UPDATE
@@ -28,9 +38,10 @@ class AttachmentRepository(Crud[Columns]):
                     url=$4
                 WHERE a.filename<>excluded.filename OR
                         a.url<>excluded.url
-        """, data)
+        """, astuple(data))
+
 
     @inject_conn
-    async def soft_delete(self, conn: DBConnection, data: Sequence[Tuple[Id]]) -> None:
+    async def soft_delete(self, conn: DBConnection, id: Id) -> None:
         # TODO: soft_delete not implemented
         raise NotImplemented
