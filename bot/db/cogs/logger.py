30c8fea56b5417e737957c6e479b2407ae813ded
zloutek1 <zloutek1@gmail.com>
2022-12-01 07:49:46 +0100

    refactor: move cog tables into cogs package

--- bot/db/cogs/logger.py
+++ bot/db/cogs/logger.py
@@ -1,39 +1,56 @@
+from dataclasses import dataclass
 from datetime import datetime
-from typing import Any, Optional, Tuple
+from typing import Optional, Tuple
 
-from .utils import DBConnection, Id, Record, Table, inject_conn
-from .tables import LOGGER
+from bot.db.utils import Id, Entity, Table, DBConnection, inject_conn
 
 
-class LoggerRepository(Table):
+
+@dataclass
+class LoggerEntity(Entity):
+    __table_name__ = "cogs.logger"
+
+    channel_id: Id
+    from_date: datetime
+    to_date: datetime
+    finished_at: Optional[datetime] = None
+
+
+
+class LoggerRepository(Table[LoggerEntity]):
     def __init__(self) -> None:
-        super().__init__(table_name=LOGGER)
+        super().__init__(entity=LoggerEntity)
+
 
     @inject_conn
     async def begin_process(self, conn: DBConnection, data: Tuple[Id, datetime, datetime]) -> None:
         channel_id, from_date, to_date = data
         await conn.execute(f"""
-            INSERT INTO {LOGGER} VALUES ($1, $2, $3, NULL)
+            INSERT INTO cogs.logger VALUES ($1, $2, $3, NULL)
             ON CONFLICT (channel_id, from_date) DO NOTHING
         """, channel_id, from_date, to_date)
+
 
     @inject_conn
     async def end_process(self, conn: DBConnection, data: Tuple[Id, datetime, datetime]) -> None:
         channel_id, from_date, to_date = data
         await conn.execute(f"""
-            UPDATE {LOGGER} 
+            UPDATE cogs.logger
             SET finished_at=NOW()
             WHERE channel_id=$1 AND from_date=$2 AND to_date=$3
         """, channel_id, from_date, to_date)
 
+
     @inject_conn
-    async def find_last_process(self, conn: DBConnection, channel_id: Id) -> Optional[Record]:
-        return await conn.fetchrow(f"""
+    async def find_last_process(self, conn: DBConnection, channel_id: Id) -> Optional[LoggerEntity]:
+        row = await conn.fetchrow(f"""
             SELECT * 
-            FROM {LOGGER}
+            FROM cogs.logger
             WHERE channel_id=$1
             ORDER BY to_date DESC
         """, channel_id)
+        return LoggerEntity.convert(row) if row else None
+
 
     @inject_conn
     async def find_updatable_processes(self, conn: DBConnection) -> list[Record]:
