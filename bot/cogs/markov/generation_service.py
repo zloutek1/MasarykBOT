3213ce8e79ba490604d0276ebe332ac2c0772583
zloutek1 <zloutek1@gmail.com>
2022-12-23 19:59:54 +0100

    fix: type error

--- bot/cogs/markov/generation_service.py
+++ bot/cogs/markov/generation_service.py
@@ -1,5 +1,5 @@
 import random
-from typing import Optional
+from typing import Optional, Tuple
 
 import inject
 from discord.utils import get
@@ -18,7 +18,7 @@ class MarkovGenerationService:
         self.markov_repository = markov_repository
 
 
-    async def generate(self, guild_id: int, start: str = 0, limit: int = 4_000) -> str:
+    async def generate(self, guild_id: int, start: str = '', limit: int = 4_000) -> str:
         (message, follows) = await self._try_to_find_start(guild_id, start)
         while follows is not None and len(message) < limit:
             message += follows
@@ -26,7 +26,7 @@ class MarkovGenerationService:
         return message
 
 
-    async def _try_to_find_start(self, guild_id: int, message: str) -> (str, str):
+    async def _try_to_find_start(self, guild_id: int, message: str) -> Tuple[str, Optional[str]]:
         follows = await self._find_next(guild_id, message)
         if not follows:
             message = ""
