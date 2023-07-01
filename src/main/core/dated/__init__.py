e191d23f14234b67cae1a185c97faeaf2eba7e58
zloutek1 <zloutek1@gmail.com>
2023-07-02 00:00:53 +0200

    docs: added some docstrings

---
+++ src/main/core/dated/__init__.py
@@ -0,0 +1,4 @@
+"""
+This module provides the functionality for soft-delete
+enabling entites to appear deleted but persist in the database
+"""
---
+++ src/main/core/domain/__init__.py
@@ -0,0 +1,4 @@
+"""
+This module specifies crud operations on a DomainEntity
+which is defined by its id
+"""
