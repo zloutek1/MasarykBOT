2b031819c802d46efd81854bbba2cc4a2e0ec3b9
zloutek1 <zloutek1@gmail.com>
2023-07-01 23:54:36 +0200

    feat: added basic app initialization

--- src/main/__main__.py
+++ src/main/__main__.py
@@ -1,1 +1,30 @@
-print("Hello world")
+import asyncio
+import os
+
+from dotenv import load_dotenv
+
+from core.bot import MasarykBot
+from core.inject import Inject
+
+
+async def migrate_db() -> None:
+    container = Inject()
+    db = container.database()
+    await db.create_database()
+
+
+async def main() -> None:
+    await migrate_db()
+
+    bot = MasarykBot()
+    await bot.load_extension("leaderboard")
+    await bot.start(os.getenv("TOKEN"))
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Shutting down")
