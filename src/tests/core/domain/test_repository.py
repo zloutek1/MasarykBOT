31697f37c5f7afd307e4ba5b1ca1e231bebc16a9
zloutek1 <zloutek1@gmail.com>
2023-07-02 02:02:02 +0200

    fix: failing tests

--- src/tests/core/domain/test_repository.py
+++ src/tests/core/domain/test_repository.py
@@ -1,15 +1,18 @@
 import unittest
+from dataclasses import dataclass
 
 from sqlalchemy import Column, String
+from sqlalchemy.orm import Mapped
 
 from core.database import Entity, Database
 from core.domain.mixin import DomainMixin
 from core.domain.repository import DomainRepository
 
 
+@dataclass
 class TestItem(Entity, DomainMixin):
     __tablename__ = "test_item"
-    name = Column(String)
+    name: Mapped[str] = Column(String)
 
 
 class TestItemRepository(DomainRepository[TestItem]):
@@ -29,31 +32,40 @@ class Test(unittest.IsolatedAsyncioTestCase):
         self.session = await self.database.session().__aenter__()
 
     async def asyncTearDown(self) -> None:
-        await self.session.rollback()
-        await self.session.close()
+        async with self.database._engine.begin() as conn:
+            await conn.run_sync(Entity.metadata.drop_all)
 
     async def test_find_all(self):
         # Create test records
-        item1 = TestItem(name='Alice')
-        item2 = TestItem(name='Bob')
+        item1 = TestItem()
+        item1.name = 'Alice'
+
+        item2 = TestItem()
+        item2.name = 'Bob'
+
         async with self.session.begin():
             self.session.add_all([item1, item2])
             await self.session.commit()
+            self.session.expunge_all()
 
         # Call the method being tested
         result = await self.repository.find_all()
 
         # Assert the expected results
+        print(result)
         self.assertEqual(len(result), 2)
         self.assertIn(item1, result)
         self.assertIn(item2, result)
 
     async def test_find(self):
         # Create a test record
-        item = TestItem(name='Alice')
+        item = TestItem()
+        item.name = 'John'
+
         async with self.session.begin():
             self.session.add(item)
             await self.session.commit()
+            self.session.expunge_all()
 
         # Call the method being tested
         result = await self.repository.find(item.id)
@@ -63,19 +75,23 @@ class Test(unittest.IsolatedAsyncioTestCase):
 
     async def test_create(self):
         # Call the method being tested
-        item = TestItem(name='Alice')
+        item = TestItem()
+        item.name = 'Josh'
         result = await self.repository.create(item)
 
         # Assert the expected result
         self.assertIsInstance(result, TestItem)
-        self.assertEqual(result.name, 'Alice')
+        self.assertEqual(result.name, 'Josh')
 
     async def test_update(self):
         # Create a test record
-        item = TestItem(name='Alice')
+        item = TestItem()
+        item.name = 'Amelia'
+
         async with self.session.begin():
             self.session.add(item)
             await self.session.commit()
+            self.session.expunge_all()
 
         # Call the method being tested
         item.name = 'Alicia'
@@ -87,10 +103,13 @@ class Test(unittest.IsolatedAsyncioTestCase):
 
     async def test_delete(self):
         # Create a test record
-        item = TestItem(name='Alice')
+        item = TestItem()
+        item.name = 'Guy'
+
         async with self.session.begin():
             self.session.add(item)
             await self.session.commit()
+            self.session.expunge_all()
 
         # Call the method being tested
         await self.repository.delete(item.id)
